2422d19bf85593ce0dac94113843d3fa39c63f2a
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-01-29 12:32:15 -0500

    Add test case to verify maximum list limit

--- src/routes/apps/{app_id}/actions/tests.rs
+++ src/routes/apps/{app_id}/actions/tests.rs
@@ -440,63 +440,66 @@ async fn verify_default_list_limit() -> Result<(), TestSlashstepServerError> {
 
 }
 
-// /// Verifies that the server returns a 422 status code when the provided limit is over the maximum limit.
-// #[tokio::test]
-// async fn verify_maximum_action_list_limit() -> Result<(), TestSlashstepServerError> {
+/// Verifies that the server returns a 422 status code when the provided limit is over the maximum limit.
+#[tokio::test]
+async fn verify_maximum_list_limit() -> Result<(), TestSlashstepServerError> {
 
-//   let test_environment = TestEnvironment::new().await?;
-//   let mut postgres_client = test_environment.postgres_pool.get().await?;
-//   initialize_required_tables(&mut postgres_client).await?;
-//   initialize_predefined_actions(&mut postgres_client).await?;
-//   initialize_predefined_roles(&mut postgres_client).await?;
+  let test_environment = TestEnvironment::new().await?;
+  let mut postgres_client = test_environment.postgres_pool.get().await?;
+  initialize_required_tables(&mut postgres_client).await?;
+  initialize_predefined_actions(&mut postgres_client).await?;
+  initialize_predefined_roles(&mut postgres_client).await?;
   
-//   // Grant access to the "slashstep.actions.get" action to the user.
-//   let user = test_environment.create_random_user().await?;
-//   let session = test_environment.create_session(&user.id).await?;
-//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
-//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
-//   let get_actions_action = Action::get_by_name("slashstep.actions.get", &mut postgres_client).await?;
-//   AccessPolicy::create(&InitialAccessPolicyProperties {
-//     action_id: get_actions_action.id,
-//     permission_level: AccessPolicyPermissionLevel::User,
-//     is_inheritance_enabled: true,
-//     principal_type: AccessPolicyPrincipalType::User,
-//     principal_user_id: Some(user.id),
-//     scoped_resource_type: AccessPolicyResourceType::Instance,
-//     ..Default::default()
-//   }, &mut postgres_client).await?;
+  // Grant access to the "slashstep.actions.get" action to the user.
+  let user = test_environment.create_random_user().await?;
+  let session = test_environment.create_session(&user.id).await?;
+  let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+  let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  let get_actions_action = Action::get_by_name("slashstep.actions.get", &mut postgres_client).await?;
+  AccessPolicy::create(&InitialAccessPolicyProperties {
+    action_id: get_actions_action.id,
+    permission_level: AccessPolicyPermissionLevel::User,
+    is_inheritance_enabled: true,
+    principal_type: AccessPolicyPrincipalType::User,
+    principal_user_id: Some(user.id),
+    scoped_resource_type: AccessPolicyResourceType::Instance,
+    ..Default::default()
+  }, &mut postgres_client).await?;
 
-//   // Grant access to the "slashstep.actions.list" action to the user.
-//   let list_actions_action = Action::get_by_name("slashstep.actions.list", &mut postgres_client).await?;
-//   AccessPolicy::create(&InitialAccessPolicyProperties {
-//     action_id: list_actions_action.id,
-//     permission_level: AccessPolicyPermissionLevel::User,
-//     is_inheritance_enabled: true,
-//     principal_type: AccessPolicyPrincipalType::User,
-//     principal_user_id: Some(user.id),
-//     scoped_resource_type: AccessPolicyResourceType::Instance,
-//     ..Default::default()
-//   }, &mut postgres_client).await?;
+  // Grant access to the "slashstep.actions.list" action to the user.
+  let list_actions_action = Action::get_by_name("slashstep.actions.list", &mut postgres_client).await?;
+  AccessPolicy::create(&InitialAccessPolicyProperties {
+    action_id: list_actions_action.id,
+    permission_level: AccessPolicyPermissionLevel::User,
+    is_inheritance_enabled: true,
+    principal_type: AccessPolicyPrincipalType::User,
+    principal_user_id: Some(user.id),
+    scoped_resource_type: AccessPolicyResourceType::Instance,
+    ..Default::default()
+  }, &mut postgres_client).await?;
 
-//   // Set up the server and send the request.
-//   let state = AppState {
-//     database_pool: test_environment.postgres_pool.clone(),
-//   };
-//   let router = super::get_router(state.clone())
-//     .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
-//     .with_state(state)
-//     .into_make_service_with_connect_info::<SocketAddr>();
-//   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/actions"))
-//     .add_query_param("query", format!("limit {}", DEFAULT_ACTION_LIST_LIMIT + 1))
-//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
-//     .await;
+  // Create dummy resources.
+  let dummy_app = test_environment.create_random_app().await?;
+
+  // Set up the server and send the request.
+  let state = AppState {
+    database_pool: test_environment.postgres_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
+    .add_query_param("query", format!("limit {}", DEFAULT_ACTION_LIST_LIMIT + 1))
+    .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+    .await;
   
-//   assert_eq!(response.status_code(), StatusCode::UNPROCESSABLE_ENTITY);
+  assert_eq!(response.status_code(), StatusCode::UNPROCESSABLE_ENTITY);
 
-//   return Ok(());
+  return Ok(());
 
-// }
+}
 
 // /// Verifies that the server returns a 400 status code when the query is invalid.
 // #[tokio::test]
@@ -548,15 +551,15 @@ async fn verify_default_list_limit() -> Result<(), TestSlashstepServerError> {
 //   let test_server = TestServer::new(router)?;
 
 //   let requests = vec![
-//     test_server.get(&format!("/actions"))
+//     test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //       .add_query_param("query", format!("app_ied = {}", get_actions_action.id)),
-//     test_server.get(&format!("/actions"))
+//     test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //       .add_query_param("query", format!("SELECT * FROM actions")),
-//     test_server.get(&format!("/actions"))
+//     test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //       .add_query_param("query", format!("1 = 1")),
-//     test_server.get(&format!("/actions"))
+//     test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //       .add_query_param("query", format!("SELECT PG_SLEEP(10)")),
-//     test_server.get(&format!("/actions"))
+//     test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //       .add_query_param("query", format!("SELECT * FROM actions WHERE id = {}", get_actions_action.id))
 //   ];
   
@@ -594,7 +597,7 @@ async fn verify_default_list_limit() -> Result<(), TestSlashstepServerError> {
 //     .with_state(state)
 //     .into_make_service_with_connect_info::<SocketAddr>();
 //   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/actions"))
+//   let response = test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //     .await;
   
 //   // Verify the response.
@@ -629,7 +632,7 @@ async fn verify_default_list_limit() -> Result<(), TestSlashstepServerError> {
 //     .with_state(state)
 //     .into_make_service_with_connect_info::<SocketAddr>();
 //   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/actions"))
+//   let response = test_server.get(&format!("/apps/{}/actions", &dummy_app.id))
 //     .add_query_param("query", format!("limit {}", DEFAULT_ACTION_LIST_LIMIT + 1))
 //     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
 //     .await;
