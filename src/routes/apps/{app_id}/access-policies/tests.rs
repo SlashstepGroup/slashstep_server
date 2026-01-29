d147068e4537952df4232dca167bcea80ba68b5a
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-01-28 19:13:24 -0500

    Add test case to verify request body JSON when creating resource

--- src/routes/apps/{app_id}/access-policies/tests.rs
+++ src/routes/apps/{app_id}/access-policies/tests.rs
@@ -98,3 +98,160 @@ async fn verify_successful_resource_creation() -> Result<(), TestSlashstepServer
   return Ok(());
   
 }
+
+/// Verifies that the server returns a 400 status code when the request body is not valid JSON.
+#[tokio::test]
+async fn verify_request_body_json_when_creating_resource() -> Result<(), TestSlashstepServerError> {
+
+  let test_environment = TestEnvironment::new().await?;
+  let mut postgres_client = test_environment.postgres_pool.get().await?;
+  initialize_required_tables(&mut postgres_client).await?;
+  initialize_predefined_actions(&mut postgres_client).await?;
+  initialize_predefined_roles(&mut postgres_client).await?;
+  
+  // Create a dummy app.
+  let dummy_app = test_environment.create_random_app().await?;
+
+  // Set up the server and send the request.
+  let state = AppState {
+    database_pool: test_environment.postgres_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.post(&format!("/apps/{}/access-policies", dummy_app.id))
+    .add_header("Content-Type", "application/json")
+    .json(&serde_json::json!({
+      "action_id": Uuid::now_v7(),
+      "permission_level": "SuperAdmin",
+      "is_inheritance_enabled": 1,
+      "principal_type": "App",
+      "principal_app_id": dummy_app.id,
+    }))
+    .await;
+  
+  // Verify the response.
+  assert_eq!(response.status_code(), StatusCode::BAD_REQUEST);
+  return Ok(());
+
+}
+
+// /// Verifies that the server returns a 401 status code when the user lacks permissions and is unauthenticated.
+// #[tokio::test]
+// async fn verify_authentication_when_creating_resource() -> Result<(), TestSlashstepServerError> {
+
+//   let test_environment = TestEnvironment::new().await?;
+//   let mut postgres_client = test_environment.postgres_pool.get().await?;
+//   initialize_required_tables(&mut postgres_client).await?;
+//   initialize_predefined_actions(&mut postgres_client).await?;
+//   initialize_predefined_roles(&mut postgres_client).await?;
+  
+//   // Create a dummy app.
+//   let dummy_app = test_environment.create_random_app().await?;
+
+//   // Set up the server and send the request.
+//   let initial_action_properties = InitialActionPropertiesForPredefinedScope {
+//     name: Uuid::now_v7().to_string(),
+//     display_name: Uuid::now_v7().to_string(),
+//     description: Uuid::now_v7().to_string()
+//   };
+//   let state = AppState {
+//     database_pool: test_environment.postgres_pool.clone(),
+//   };
+//   let router = super::get_router(state.clone())
+//     .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+//     .with_state(state)
+//     .into_make_service_with_connect_info::<SocketAddr>();
+//   let test_server = TestServer::new(router)?;
+//   let response = test_server.post(&format!("/apps/{}/actions", dummy_app.id))
+//     .add_header("Content-Type", "application/json")
+//     .json(&serde_json::json!(initial_action_properties))
+//     .await;
+  
+//   // Verify the response.
+//   assert_eq!(response.status_code(), 401);
+//   return Ok(());
+
+// }
+
+// /// Verifies that the server returns a 403 status code when the user lacks permissions and is authenticated.
+// #[tokio::test]
+// async fn verify_permission_when_creating_resource() -> Result<(), TestSlashstepServerError> {
+
+//   let test_environment = TestEnvironment::new().await?;
+//   let mut postgres_client = test_environment.postgres_pool.get().await?;
+//   initialize_required_tables(&mut postgres_client).await?;
+//   initialize_predefined_actions(&mut postgres_client).await?;
+//   initialize_predefined_roles(&mut postgres_client).await?;
+
+//   // Create the user and the session.
+//   let user = test_environment.create_random_user().await?;
+//   let session = test_environment.create_session(&user.id).await?;
+//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  
+//   // Create a dummy app.
+//   let dummy_app = test_environment.create_random_app().await?;
+
+//   // Set up the server and send the request.
+//   let initial_action_properties = InitialActionPropertiesForPredefinedScope {
+//     name: Uuid::now_v7().to_string(),
+//     display_name: Uuid::now_v7().to_string(),
+//     description: Uuid::now_v7().to_string()
+//   };
+//   let state = AppState {
+//     database_pool: test_environment.postgres_pool.clone(),
+//   };
+//   let router = super::get_router(state.clone())
+//     .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+//     .with_state(state)
+//     .into_make_service_with_connect_info::<SocketAddr>();
+//   let test_server = TestServer::new(router)?;
+//   let response = test_server.post(&format!("/apps/{}/actions", dummy_app.id))
+//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+//     .add_header("Content-Type", "application/json")
+//     .json(&serde_json::json!(initial_action_properties))
+//     .await;
+  
+//   // Verify the response.
+//   assert_eq!(response.status_code(), StatusCode::FORBIDDEN);
+//   return Ok(());
+
+// }
+
+// /// Verifies that the router can return a 404 status code if the requested resource doesn't exist.
+// #[tokio::test]
+// #[timeout(20000)]
+// async fn verify_not_found_when_creating_resource() -> Result<(), TestSlashstepServerError> {
+
+//   let test_environment = TestEnvironment::new().await?;
+//   initialize_required_tables(&mut test_environment.postgres_pool.get().await?).await?;
+//   initialize_predefined_actions(&mut test_environment.postgres_pool.get().await?).await?;
+//   initialize_predefined_roles(&mut test_environment.postgres_pool.get().await?).await?;
+
+//   let initial_action_properties = InitialActionPropertiesForPredefinedScope {
+//     name: Uuid::now_v7().to_string(),
+//     display_name: Uuid::now_v7().to_string(),
+//     description: Uuid::now_v7().to_string()
+//   };
+
+//   let state = AppState {
+//     database_pool: test_environment.postgres_pool.clone(),
+//   };
+//   let router = super::get_router(state.clone())
+//     .layer(middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request))
+//     .with_state(state)
+//     .into_make_service_with_connect_info::<SocketAddr>();
+//   let test_server = TestServer::new(router)?;
+
+//   let response = test_server.post(&format!("/apps/{}/actions", uuid::Uuid::now_v7()))
+//     .add_header("Content-Type", "application/json")
+//     .json(&serde_json::json!(initial_action_properties))
+//     .await;
+  
+//   assert_eq!(response.status_code(), 404);
+//   return Ok(());
+
+// }
