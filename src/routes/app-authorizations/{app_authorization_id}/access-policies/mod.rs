02b8cc82f2f604a498dd323c7bed6aa2f2a05d97
Christian Toney <37517470+Christian-Toney@users.noreply.github.com>
2026-01-30 19:52:54 -0500

    Add feature: Create access policies of app authorization (#254)

--- src/routes/app-authorizations/{app_authorization_id}/access-policies/mod.rs
+++ src/routes/app-authorizations/{app_authorization_id}/access-policies/mod.rs
@@ -13,10 +13,10 @@
 mod tests;
 
 use std::sync::Arc;
-use axum::{Extension, Router, extract::{Path, Query, State}};
+use axum::{Extension, Json, Router, extract::{Path, Query, State, rejection::JsonRejection}};
 use axum_extra::response::ErasedJson;
 use pg_escape::quote_literal;
-use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::AccessPolicyResourceType, action_log_entry::ActionLogEntryTargetResourceType, app::App, http_transaction::HTTPTransaction, user::User}, utilities::{reusable_route_handlers::{AccessPolicyListQueryParameters, list_access_policies}, route_handler_utilities::{get_app_authorization_from_id, get_resource_hierarchy}}};
+use crate::{AppState, HTTPError, middleware::{authentication_middleware, http_request_middleware}, resources::{access_policy::{AccessPolicy, AccessPolicyPermissionLevel, AccessPolicyResourceType, InitialAccessPolicyProperties, InitialAccessPolicyPropertiesForPredefinedScope}, action_log_entry::{ActionLogEntry, ActionLogEntryActorType, ActionLogEntryTargetResourceType, InitialActionLogEntryProperties}, app::App, http_transaction::HTTPTransaction, server_log_entry::ServerLogEntry, user::User}, utilities::{reusable_route_handlers::{AccessPolicyListQueryParameters, list_access_policies}, route_handler_utilities::{AuthenticatedPrincipal, get_action_from_id, get_action_from_name, get_app_authorization_from_id, get_authenticated_principal, get_resource_hierarchy, verify_principal_permissions}}};
 
 /// GET /app-authorizations/{app_authorization_id}/access-policies
 /// 
@@ -50,111 +50,111 @@ async fn handle_list_access_policies_request(
 
 }
 
-// /// POST /app-authorizations/{app_authorization_id}/access-policies
-// /// 
-// /// Creates an access policy for an app authorization.
-// #[axum::debug_handler]
-// async fn handle_create_access_policy_request(
-//   Path(action_id): Path<String>,
-//   State(state): State<AppState>, 
-//   Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
-//   Extension(user): Extension<Option<Arc<User>>>,
-//   Extension(app): Extension<Option<Arc<App>>>,
-//   body: Result<Json<InitialAccessPolicyPropertiesForPredefinedScope>, JsonRejection>
-// ) -> Result<Json<AccessPolicy>, HTTPError> {
+/// POST /app-authorizations/{app_authorization_id}/access-policies
+/// 
+/// Creates an access policy for an app authorization.
+#[axum::debug_handler]
+async fn handle_create_access_policy_request(
+  Path(app_authorization_id): Path<String>,
+  State(state): State<AppState>, 
+  Extension(http_transaction): Extension<Arc<HTTPTransaction>>,
+  Extension(authenticated_user): Extension<Option<Arc<User>>>,
+  Extension(authenticated_app): Extension<Option<Arc<App>>>,
+  body: Result<Json<InitialAccessPolicyPropertiesForPredefinedScope>, JsonRejection>
+) -> Result<Json<AccessPolicy>, HTTPError> {
 
-//   let http_transaction = http_transaction.clone();
+  let http_transaction = http_transaction.clone();
 
-//   // Verify the request body.
-//   ServerLogEntry::trace("Verifying request body...", Some(&http_transaction.id), &state.database_pool).await.ok();
-//   let access_policy_properties_json = match body {
+  // Verify the request body.
+  ServerLogEntry::trace("Verifying request body...", Some(&http_transaction.id), &state.database_pool).await.ok();
+  let access_policy_properties_json = match body {
 
-//     Ok(access_policy_properties_json) => access_policy_properties_json,
+    Ok(access_policy_properties_json) => access_policy_properties_json,
 
-//     Err(error) => {
+    Err(error) => {
 
-//       let http_error = match error {
+      let http_error = match error {
 
-//         JsonRejection::JsonDataError(error) => HTTPError::BadRequestError(Some(error.to_string())),
+        JsonRejection::JsonDataError(error) => HTTPError::BadRequestError(Some(error.to_string())),
 
-//         JsonRejection::JsonSyntaxError(_) => HTTPError::BadRequestError(Some(format!("Failed to parse request body. Ensure the request body is valid JSON."))),
+        JsonRejection::JsonSyntaxError(_) => HTTPError::BadRequestError(Some(format!("Failed to parse request body. Ensure the request body is valid JSON."))),
 
-//         JsonRejection::MissingJsonContentType(_) => HTTPError::BadRequestError(Some(format!("Missing request body content type. It should be \"application/json\"."))),
+        JsonRejection::MissingJsonContentType(_) => HTTPError::BadRequestError(Some(format!("Missing request body content type. It should be \"application/json\"."))),
 
-//         JsonRejection::BytesRejection(error) => HTTPError::InternalServerError(Some(format!("Failed to parse request body: {:?}", error))),
+        JsonRejection::BytesRejection(error) => HTTPError::InternalServerError(Some(format!("Failed to parse request body: {:?}", error))),
 
-//         _ => HTTPError::InternalServerError(Some(error.to_string()))
+        _ => HTTPError::InternalServerError(Some(error.to_string()))
 
-//       };
+      };
       
-//       http_error.print_and_save(Some(&http_transaction.id), &state.database_pool).await.ok();
-//       return Err(http_error);
+      http_error.print_and_save(Some(&http_transaction.id), &state.database_pool).await.ok();
+      return Err(http_error);
 
-//     }
+    }
 
-//   };
+  };
 
-//   // Make sure the user can create access policies for the target action.
-//   let target_action = get_action_from_id(&action_id, &http_transaction, &state.database_pool).await?;
-//   let resource_hierarchy = get_resource_hierarchy(&target_action, &AccessPolicyResourceType::Action, &target_action.id, &http_transaction, &state.database_pool).await?;
-//   let create_access_policies_action = get_action_from_name("slashstep.accessPolicies.create", &http_transaction, &state.database_pool).await?;
-//   let authenticated_principal = get_authenticated_principal(&user, &app)?;
-//   verify_principal_permissions(&authenticated_principal, &create_access_policies_action, &resource_hierarchy, &http_transaction, &AccessPolicyPermissionLevel::User, &state.database_pool).await?;
+  // Make sure the user can create access policies for the target action.
+  let target_app_authorization = get_app_authorization_from_id(&app_authorization_id, &http_transaction, &state.database_pool).await?;
+  let resource_hierarchy = get_resource_hierarchy(&target_app_authorization, &AccessPolicyResourceType::AppAuthorization, &target_app_authorization.id, &http_transaction, &state.database_pool).await?;
+  let create_access_policies_action = get_action_from_name("slashstep.accessPolicies.create", &http_transaction, &state.database_pool).await?;
+  let authenticated_principal = get_authenticated_principal(&authenticated_user, &authenticated_app)?;
+  verify_principal_permissions(&authenticated_principal, &create_access_policies_action, &resource_hierarchy, &http_transaction, &AccessPolicyPermissionLevel::User, &state.database_pool).await?;
 
-//   // Make sure the user has at least editor access to the access policy's action.
-//   let access_policy_action = get_action_from_id(&access_policy_properties_json.action_id.to_string(), &http_transaction, &state.database_pool).await?;
-//   let minimum_permission_level = if access_policy_properties_json.permission_level > AccessPolicyPermissionLevel::Editor { access_policy_properties_json.permission_level } else { AccessPolicyPermissionLevel::Editor };
-//   verify_principal_permissions(&authenticated_principal, &access_policy_action, &resource_hierarchy, &http_transaction, &minimum_permission_level, &state.database_pool).await?;
+  // Make sure the user has at least editor access to the access policy's action.
+  let access_policy_action = get_action_from_id(&access_policy_properties_json.action_id.to_string(), &http_transaction, &state.database_pool).await?;
+  let minimum_permission_level = if access_policy_properties_json.permission_level > AccessPolicyPermissionLevel::Editor { access_policy_properties_json.permission_level } else { AccessPolicyPermissionLevel::Editor };
+  verify_principal_permissions(&authenticated_principal, &access_policy_action, &resource_hierarchy, &http_transaction, &minimum_permission_level, &state.database_pool).await?;
 
-//   // Create the access policy.
-//   ServerLogEntry::trace(&format!("Creating access policy for action {}...", action_id), Some(&http_transaction.id), &state.database_pool).await.ok();
-//   let access_policy = match AccessPolicy::create(&InitialAccessPolicyProperties {
-//     action_id: access_policy_properties_json.action_id,
-//     permission_level: access_policy_properties_json.permission_level,
-//     is_inheritance_enabled: access_policy_properties_json.is_inheritance_enabled,
-//     principal_type: access_policy_properties_json.principal_type,
-//     principal_user_id: access_policy_properties_json.principal_user_id,
-//     principal_group_id: access_policy_properties_json.principal_group_id,
-//     principal_role_id: access_policy_properties_json.principal_role_id,
-//     principal_app_id: access_policy_properties_json.principal_app_id,
-//     scoped_resource_type: AccessPolicyResourceType::Action,
-//     scoped_action_id: Some(target_action.id),
-//     ..Default::default()
-//   }, &state.database_pool).await {
+  // Create the access policy.
+  ServerLogEntry::trace(&format!("Creating access policy for app authorization {}...", app_authorization_id), Some(&http_transaction.id), &state.database_pool).await.ok();
+  let access_policy = match AccessPolicy::create(&InitialAccessPolicyProperties {
+    action_id: access_policy_properties_json.action_id,
+    permission_level: access_policy_properties_json.permission_level,
+    is_inheritance_enabled: access_policy_properties_json.is_inheritance_enabled,
+    principal_type: access_policy_properties_json.principal_type,
+    principal_user_id: access_policy_properties_json.principal_user_id,
+    principal_group_id: access_policy_properties_json.principal_group_id,
+    principal_role_id: access_policy_properties_json.principal_role_id,
+    principal_app_id: access_policy_properties_json.principal_app_id,
+    scoped_resource_type: AccessPolicyResourceType::AppAuthorization,
+    scoped_app_authorization_id: Some(target_app_authorization.id),
+    ..Default::default()
+  }, &state.database_pool).await {
 
-//     Ok(access_policy) => access_policy,
+    Ok(access_policy) => access_policy,
 
-//     Err(error) => {
+    Err(error) => {
 
-//       let http_error = HTTPError::InternalServerError(Some(format!("Failed to create access policy: {:?}", error)));
-//       http_error.print_and_save(Some(&http_transaction.id), &state.database_pool).await.ok();
-//       return Err(http_error)
+      let http_error = HTTPError::InternalServerError(Some(format!("Failed to create access policy: {:?}", error)));
+      http_error.print_and_save(Some(&http_transaction.id), &state.database_pool).await.ok();
+      return Err(http_error)
 
-//     }
+    }
 
-//   };
+  };
 
-//   ActionLogEntry::create(&InitialActionLogEntryProperties {
-//     action_id: create_access_policies_action.id,
-//     http_transaction_id: Some(http_transaction.id),
-//     actor_type: if let AuthenticatedPrincipal::User(_) = &authenticated_principal { ActionLogEntryActorType::User } else { ActionLogEntryActorType::App },
-//     actor_user_id: if let AuthenticatedPrincipal::User(user) = &authenticated_principal { Some(user.id.clone()) } else { None },
-//     actor_app_id: if let AuthenticatedPrincipal::App(app) = &authenticated_principal { Some(app.id.clone()) } else { None },
-//     target_resource_type: ActionLogEntryTargetResourceType::AccessPolicy,
-//     target_access_policy_id: Some(access_policy.id),
-//     ..Default::default()
-//   }, &state.database_pool).await.ok();
-//   ServerLogEntry::success(&format!("Successfully created access policy {}.", access_policy.id), Some(&http_transaction.id), &state.database_pool).await.ok();
+  ActionLogEntry::create(&InitialActionLogEntryProperties {
+    action_id: create_access_policies_action.id,
+    http_transaction_id: Some(http_transaction.id),
+    actor_type: if let AuthenticatedPrincipal::User(_) = &authenticated_principal { ActionLogEntryActorType::User } else { ActionLogEntryActorType::App },
+    actor_user_id: if let AuthenticatedPrincipal::User(user) = &authenticated_principal { Some(user.id.clone()) } else { None },
+    actor_app_id: if let AuthenticatedPrincipal::App(app) = &authenticated_principal { Some(app.id.clone()) } else { None },
+    target_resource_type: ActionLogEntryTargetResourceType::AccessPolicy,
+    target_access_policy_id: Some(access_policy.id),
+    ..Default::default()
+  }, &state.database_pool).await.ok();
+  ServerLogEntry::success(&format!("Successfully created access policy {}.", access_policy.id), Some(&http_transaction.id), &state.database_pool).await.ok();
 
-//   return Ok(Json(access_policy));
+  return Ok(Json(access_policy));
 
-// }
+}
 
 pub fn get_router(state: AppState) -> Router<AppState> {
 
   let router = Router::<AppState>::new()
     .route("/app-authorizations/{app_authorization_id}/access-policies", axum::routing::get(handle_list_access_policies_request))
-    // .route("/actions/{action_id}/access-policies", axum::routing::post(handle_create_access_policy_request))
+    .route("/app-authorizations/{app_authorization_id}/access-policies", axum::routing::post(handle_create_access_policy_request))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_user))
     .layer(axum::middleware::from_fn_with_state(state.clone(), authentication_middleware::authenticate_app))
     .layer(axum::middleware::from_fn_with_state(state.clone(), http_request_middleware::create_http_request));
--- src/routes/app-authorizations/{app_authorization_id}/access-policies/tests.rs
+++ src/routes/app-authorizations/{app_authorization_id}/access-policies/tests.rs
@@ -15,7 +15,7 @@ use axum_test::TestServer;
 use pg_escape::quote_literal;
 use reqwest::StatusCode;
 use uuid::Uuid;
-use crate::{AppState, initialize_required_tables, predefinitions::{initialize_predefined_actions, initialize_predefined_roles}, resources::{access_policy::{AccessPolicy, AccessPolicyPermissionLevel, DEFAULT_ACCESS_POLICY_LIST_LIMIT, IndividualPrincipal, InitialAccessPolicyProperties}, action::Action, session::Session}, tests::{TestEnvironment, TestSlashstepServerError}, utilities::reusable_route_handlers::ListAccessPolicyResponseBody};
+use crate::{AppState, initialize_required_tables, predefinitions::{initialize_predefined_actions, initialize_predefined_roles}, resources::{access_policy::{AccessPolicy, AccessPolicyPermissionLevel, AccessPolicyPrincipalType, DEFAULT_ACCESS_POLICY_LIST_LIMIT, IndividualPrincipal, InitialAccessPolicyProperties, InitialAccessPolicyPropertiesForPredefinedScope}, action::Action, session::Session}, tests::{TestEnvironment, TestSlashstepServerError}, utilities::reusable_route_handlers::ListAccessPolicyResponseBody};
 
 async fn create_app_authorization_access_policy(database_pool: &deadpool_postgres::Pool, scoped_app_authorization_id: &Uuid, user_id: &Uuid, action_id: &Uuid, permission_level: &AccessPolicyPermissionLevel) -> Result<AccessPolicy, TestSlashstepServerError> {
 
@@ -34,58 +34,59 @@ async fn create_app_authorization_access_policy(database_pool: &deadpool_postgre
 
 }
 
-// #[tokio::test]
-// async fn verify_successful_access_policy_creation() -> Result<(), TestSlashstepServerError> {
+#[tokio::test]
+async fn verify_successful_access_policy_creation() -> Result<(), TestSlashstepServerError> {
 
-//   let test_environment = TestEnvironment::new().await?;
-//   initialize_required_tables(&test_environment.database_pool).await?;
-//   initialize_predefined_actions(&test_environment.database_pool).await?;
+  let test_environment = TestEnvironment::new().await?;
+  initialize_required_tables(&test_environment.database_pool).await?;
+  initialize_predefined_actions(&test_environment.database_pool).await?;
 
-//   // Give the user access to the "slashstep.accessPolicies.create" action.
-//   let user = test_environment.create_random_user().await?;
-//   let session = test_environment.create_session(&user.id).await?;
-//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
-//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
-//   let create_access_policies_action = Action::get_by_name("slashstep.accessPolicies.create", &test_environment.database_pool).await?;
-//   test_environment.create_instance_access_policy(&user.id, &create_access_policies_action.id, &AccessPolicyPermissionLevel::User).await?;
+  // Give the user access to the "slashstep.accessPolicies.create" action.
+  let user = test_environment.create_random_user().await?;
+  let session = test_environment.create_session(&user.id).await?;
+  let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+  let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  let create_access_policies_action = Action::get_by_name("slashstep.accessPolicies.create", &test_environment.database_pool).await?;
+  test_environment.create_instance_access_policy(&user.id, &create_access_policies_action.id, &AccessPolicyPermissionLevel::User).await?;
   
-//   // Give the user editor access to a dummy action.
-//   let dummy_app_authorization = test_environment.create_random_app_authorization(&None).await?;
-//   test_environment.create_instance_access_policy(&user.id, &dummy_app_authorization.id, &AccessPolicyPermissionLevel::Editor).await?;
+  // Give the user editor access to a dummy action.
+  let dummy_app_authorization = test_environment.create_random_app_authorization(&None).await?;
+  let dummy_action = test_environment.create_random_action(&None).await?;
+  test_environment.create_instance_access_policy(&user.id, &dummy_action.id, &AccessPolicyPermissionLevel::Editor).await?;
 
-//   // Set up the server and send the request.
-//   let initial_access_policy_properties = InitialAccessPolicyPropertiesForPredefinedScope {
-//     action_id: dummy_app_authorization.id,
-//     permission_level: AccessPolicyPermissionLevel::User,
-//     is_inheritance_enabled: true,
-//     principal_type: AccessPolicyPrincipalType::User,
-//     principal_user_id: Some(user.id),
-//     ..Default::default()
-//   };
-//   let state = AppState {
-//     database_pool: test_environment.database_pool.clone(),
-//   };
-//   let router = super::get_router(state.clone())
-//     .with_state(state)
-//     .into_make_service_with_connect_info::<SocketAddr>();
-//   let test_server = TestServer::new(router)?;
-//   let response = test_server.post(&format!("/app-authorizations/{}/access-policies", dummy_app_authorization.id))
-//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
-//     .json(&serde_json::json!(initial_access_policy_properties))
-//     .await;
+  // Set up the server and send the request.
+  let initial_access_policy_properties = InitialAccessPolicyPropertiesForPredefinedScope {
+    action_id: dummy_action.id,
+    permission_level: AccessPolicyPermissionLevel::User,
+    is_inheritance_enabled: true,
+    principal_type: AccessPolicyPrincipalType::User,
+    principal_user_id: Some(user.id),
+    ..Default::default()
+  };
+  let state = AppState {
+    database_pool: test_environment.database_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.post(&format!("/app-authorizations/{}/access-policies", dummy_app_authorization.id))
+    .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+    .json(&serde_json::json!(initial_access_policy_properties))
+    .await;
   
-//   assert_eq!(response.status_code(), 200);
+  assert_eq!(response.status_code(), 200);
 
-//   let response_access_policy: AccessPolicy = response.json();
-//   assert_eq!(initial_access_policy_properties.action_id, response_access_policy.action_id);
-//   assert_eq!(initial_access_policy_properties.principal_type, response_access_policy.principal_type);
-//   assert_eq!(initial_access_policy_properties.principal_user_id, response_access_policy.principal_user_id);
-//   assert_eq!(initial_access_policy_properties.permission_level, response_access_policy.permission_level);
-//   assert_eq!(initial_access_policy_properties.is_inheritance_enabled, response_access_policy.is_inheritance_enabled);
+  let response_access_policy: AccessPolicy = response.json();
+  assert_eq!(initial_access_policy_properties.action_id, response_access_policy.action_id);
+  assert_eq!(initial_access_policy_properties.principal_type, response_access_policy.principal_type);
+  assert_eq!(initial_access_policy_properties.principal_user_id, response_access_policy.principal_user_id);
+  assert_eq!(initial_access_policy_properties.permission_level, response_access_policy.permission_level);
+  assert_eq!(initial_access_policy_properties.is_inheritance_enabled, response_access_policy.is_inheritance_enabled);
 
-//   return Ok(());
+  return Ok(());
   
-// }
+}
 
 /// Verifies that the router can return a 200 status code and the requested access policy list.
 #[tokio::test]
