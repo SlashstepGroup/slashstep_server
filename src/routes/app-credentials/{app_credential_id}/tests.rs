8d70de554b07d732e5d555cad076cd356923b5fe
Christian <37517470+Christian-Toney@users.noreply.github.com>
2026-02-01 11:49:17 -0500

    Add test case to verify permission when getting resource by ID

--- src/routes/app-credentials/{app_credential_id}/tests.rs
+++ src/routes/app-credentials/{app_credential_id}/tests.rs
@@ -126,40 +126,40 @@ async fn verify_authentication_when_getting_resource_by_id() -> Result<(), TestS
 
 }
 
-// /// Verifies that the router can return a 403 status code if the user does not have permission to view the resource.
-// #[tokio::test]
-// #[timeout(20000)]
-// async fn verify_permission_when_getting_resource_by_id() -> Result<(), TestSlashstepServerError> {
+/// Verifies that the router can return a 403 status code if the user does not have permission to view the resource.
+#[tokio::test]
+#[timeout(20000)]
+async fn verify_permission_when_getting_resource_by_id() -> Result<(), TestSlashstepServerError> {
 
-//   let test_environment = TestEnvironment::new().await?;
-//   initialize_required_tables(&test_environment.database_pool).await?;
-//   initialize_predefined_actions(&test_environment.database_pool).await?;
-//   initialize_predefined_roles(&test_environment.database_pool).await?;
+  let test_environment = TestEnvironment::new().await?;
+  initialize_required_tables(&test_environment.database_pool).await?;
+  initialize_predefined_actions(&test_environment.database_pool).await?;
+  initialize_predefined_roles(&test_environment.database_pool).await?;
 
-//   // Create the user, the session, and the resource.
-//   let user = test_environment.create_random_user().await?;
-//   let session = test_environment.create_session(&user.id).await?;
-//   let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
-//   let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
-//   let app_credential = test_environment.create_random_app_credential(&None).await?;
+  // Create the user, the session, and the resource.
+  let user = test_environment.create_random_user().await?;
+  let session = test_environment.create_session(&user.id).await?;
+  let json_web_token_private_key = Session::get_json_web_token_private_key().await?;
+  let session_token = session.generate_json_web_token(&json_web_token_private_key).await?;
+  let app_credential = test_environment.create_random_app_credential(&None).await?;
 
-//   // Set up the server and send the request.
-//   let state = AppState {
-//     database_pool: test_environment.database_pool.clone(),
-//   };
-//   let router = super::get_router(state.clone())
-//     .with_state(state)
-//     .into_make_service_with_connect_info::<SocketAddr>();
-//   let test_server = TestServer::new(router)?;
-//   let response = test_server.get(&format!("/app-credentials/{}", app_credential.id))
-//     .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
-//     .await;
+  // Set up the server and send the request.
+  let state = AppState {
+    database_pool: test_environment.database_pool.clone(),
+  };
+  let router = super::get_router(state.clone())
+    .with_state(state)
+    .into_make_service_with_connect_info::<SocketAddr>();
+  let test_server = TestServer::new(router)?;
+  let response = test_server.get(&format!("/app-credentials/{}", app_credential.id))
+    .add_cookie(Cookie::new("sessionToken", format!("Bearer {}", session_token)))
+    .await;
   
-//   // Verify the response.
-//   assert_eq!(response.status_code(), 403);
-//   return Ok(());
+  // Verify the response.
+  assert_eq!(response.status_code(), 403);
+  return Ok(());
 
-// }
+}
 
 // /// Verifies that the router can return a 404 status code if the requested resource doesn't exist.
 // #[tokio::test]
