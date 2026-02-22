afe1d3f764d31a29f9f946a57e5355de46c74ff1
Christian Toney <37517470+Christian-Toney@users.noreply.github.com>
2026-02-22 18:39:50 -0500

    Add configuration to define maximum duration for app authorization credential access tokens (#409)

--- src/predefinitions.rs
+++ src/predefinitions.rs
@@ -894,6 +894,13 @@ pub async fn initialize_predefined_configurations(database_pool: &deadpool_postg
       ..Default::default()
     },
     InitialConfigurationProperties {
+      name: "slashstep.appAuthorizationCredentials.accessTokenMaximumLifetimeMilliseconds".to_string(),
+      description: Some("The maximum lifetime of app authorization credentials access tokens in milliseconds. Slashstep Group recommends keeping this value small, as OAuth access tokens should be short-lived.".to_string()),
+      value_type: ConfigurationValueType::Number,
+      default_number_value: Some(Decimal::from(28800000 as i64)), // 8 hours in milliseconds
+      ..Default::default()
+    },
+    InitialConfigurationProperties {
       name: "slashstep.apps.maximumDisplayNameLength".to_string(),
       description: Some("The maximum length of app display names. Slashstep Group recommends setting this to a reasonable value to prevent abuse.".to_string()),
       value_type: ConfigurationValueType::Number,
